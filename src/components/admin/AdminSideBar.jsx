f9c9fa3293f9a0b9cbd833b22cff11c2b35c21da
Begimai7 <122358771+Begimai7@users.noreply.github.com>
2024-01-17 14:43:53 +0600

    Update AdminSideBar.jsx

--- src/components/admin/AdminSideBar.jsx
+++ src/components/admin/AdminSideBar.jsx
@@ -1,70 +1,68 @@
-import * as React from 'react';
-import {styled, Box} from '@mui/material';
-import Drawer from '@mui/material/Drawer';
-import CssBaseline from '@mui/material/CssBaseline';
-import AppBar from '@mui/material/AppBar';
-import Toolbar from '@mui/material/Toolbar';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemText from '@mui/material/ListItemText';
-import exit from '../../assets/icon/SignOut.svg'
+import * as React from 'react'
+import { styled, Box } from '@mui/material'
+import Drawer from '@mui/material/Drawer'
+import CssBaseline from '@mui/material/CssBaseline'
+import AppBar from '@mui/material/AppBar'
+import Toolbar from '@mui/material/Toolbar'
+import List from '@mui/material/List'
+import ListItem from '@mui/material/ListItem'
+import ListItemText from '@mui/material/ListItemText'
+import exit from '../../assets/icon/exist.svg'
+import { sidebarMenu } from '../../utils/constants'
 
+const drawerWidth = 202
 
-
-
-const drawerWidth = 202;
-
-export default function PermanentDrawerLeft() {
-  return (
-    <Box sx={{ display: 'flex' }}>
-      <CssBaseline />
-      <AppBar
-        position="fixed"
-        sx={{ width: `calc(100% - ${drawerWidth}px)`, ml: `${drawerWidth}px` }}
-      >
-
-      </AppBar>
-      <Drawer 
-        sx={{
-            backgroundColor:'red',
-            width: drawerWidth,
-            flexShrink: 0,
-            '& .MuiDrawer-paper': {
-            boxShadow:' 0px 4px 5px 0px rgba(136, 118, 197, 0.20)',
-            width: drawerWidth,
-            boxSizing: 'border-box',
-          },
-        }}
-        variant="permanent"
-        anchor="left"
-      >
-     <DrawerStyle>
-        <div>
-            <Toolbar>
-                <LogotipeAroma>Aroma<LogotipeCafe>cafe</LogotipeCafe></LogotipeAroma>
-                
-            </Toolbar>
-            <List>
-            {['Меню', 'Склад', 'Филиалы', 'Сотрудники'].map((text) => (
-                <ListItemStyled key={text} disablePadding>
-                <ListItemButtonStyle>
-                    <ListItemTextStyle primary={text} />
-                </ListItemButtonStyle>
-                </ListItemStyled>
-            ))}
-            </List>
-        </div>
-        <SignOutDiv>
-            <span>Выход</span>
-            <img src={exit} alt="" />
-        </SignOutDiv>
-     </DrawerStyle>
-      </Drawer>
-    </Box>
-  );
+export default function SideBar({ existHandler }) {
+    return (
+        <Box sx={{ display: 'flex' }}>
+            <CssBaseline />
+            <AppBar
+                position="fixed"
+                sx={{
+                    width: `calc(100% - ${drawerWidth}px)`,
+                    ml: `${drawerWidth}px`,
+                }}
+            ></AppBar>
+            <Drawer
+                sx={{
+                    backgroundColor: 'red',
+                    width: drawerWidth,
+                    flexShrink: 0,
+                    '& .MuiDrawer-paper': {
+                        boxShadow: ' 0px 4px 5px 0px rgba(136, 118, 197, 0.20)',
+                        width: drawerWidth,
+                        boxSizing: 'border-box',
+                    },
+                }}
+                variant="permanent"
+                anchor="left"
+            >
+                <DrawerStyle>
+                    <div>
+                        <Toolbar>
+                            <LogotipeAroma>
+                                Aroma<LogotipeCafe>cafe</LogotipeCafe>
+                            </LogotipeAroma>
+                        </Toolbar>
+                        <List>
+                            {sidebarMenu.map((el) => (
+                                <ListItemStyled key={el.id} disablePadding>
+                                    <ListItemButtonStyle>
+                                        <ListItemTextStyle primary={el.name} />
+                                    </ListItemButtonStyle>
+                                </ListItemStyled>
+                            ))}
+                        </List>
+                    </div>
+                    <SignOutDiv onClick={existHandler}>
+                        <span>Выход</span>
+                        <img src={exit} alt="" />
+                    </SignOutDiv>
+                </DrawerStyle>
+            </Drawer>
+        </Box>
+    )
 }
-
-
 
 const SignOutDiv = styled('div')`
     display: flex;
@@ -77,34 +75,32 @@ const SignOutDiv = styled('div')`
     cursor: pointer;
 `
 
-
 const ListItemStyled = styled(ListItem)(() => ({
-    paddingLeft:'10px',
-
+    paddingLeft: '10px',
 }))
 
-
 const ListItemButtonStyle = styled('button')(() => ({
-  flexGrow: '0',
-  background:'none',
-  backgroundColor:'white',
-  border:'none',
-  padding:'8px 16px',
-
-
-
+    flexGrow: '0',
+    background: 'none',
+    backgroundColor: 'white',
+    border: 'none',
+    padding: '8px 16px',
 }))
 
 const ListItemTextStyle = styled(ListItemText)(() => ({
-  borderBottom:'2px solid  #ffffff0',
-  
-  ':hover':{
-    transition: '0.1s',
-    borderBottom:'2px solid #00315D',
-    color:'#00315D',
-    cursor:'pointer',
-  },
+    borderBottom: '2px solid  #ffffff0',
+    fontWeight: 900,
+    fontSize: '18px',
 
+    ':hover': {
+        transition: '0.1s',
+        borderBottom: '2px solid #00315D',
+        color: '#00315D',
+        cursor: 'pointer',
+        fontSize: '18px',
+        fontWeight: 900,
+        fontFamily: 'Nunito Sans',
+    },
 }))
 const DrawerStyle = styled('div')(() => ({
     display: 'flex',
@@ -126,7 +122,7 @@ const LogotipeAroma = styled('span')`
     letter-spacing: -0.279px;
 `
 const LogotipeCafe = styled('span')`
-    color: #8876C5;
+    color: #8876c5;
     font-family: Bai Jamjuree;
     font-size: 27.936px;
     font-style: normal;
