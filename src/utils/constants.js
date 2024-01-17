d0d72d99eddf467b9e24afef942aebc315cdb73d
DI_RAIZEL09 <di.raizel.090204@gmail.com>
2024-01-17 16:57:04 +0600

    finish table

---
+++ src/utils/constants.js
@@ -0,0 +1,219 @@
+
+export const titlesMenuHeaderTable = [
+    {
+        id: 'one',
+        label: '№',
+        numeric: true,
+        width: 5,
+        align: 'right',
+        format: (value) => value.toLocaleString('en-US'),
+    },
+    {
+        id: 'two',
+        label: 'Наименование',
+        numeric: true,
+        width: 10,
+        align: 'right',
+           format: (value) => value.toLocaleString('en-US'),
+    },
+    {
+        id: 'three',
+        label: 'Категория',
+        numeric: true,
+        width: 30,
+        align: 'right',
+        format: (value) => value.toLocaleString('en-US'),
+    },
+    {
+        id: 'four',
+        label: 'Состав блюда и граммовка',
+        numeric: true,
+        width: 40,
+        align: 'right',
+        format: (value) => value.toLocaleString('en-US'),
+    },
+    {
+        id: 'five',
+        label: 'Стоимость',
+        numeric: true,
+        width: 40,
+        align: 'right',
+        format: (value) => value.toLocaleString('en-US'),
+    },
+    {
+        id: 'six',
+        label: 'Ред.',
+        numeric: true,
+        width: 40,
+        align: 'right',
+        format: (value) => value.toLocaleString('en-US'),
+    },
+ 
+]
+export const titlesSkladHeaderTable = [
+    {
+        id: 1,
+        label: '№',
+        numeric: true,
+        width: 5,
+    },
+    {
+        id:2,
+        label: 'Наименование',
+        numeric: true,
+        width: 10,
+    },
+    {
+        id: 3,
+        label: 'Количество',
+        numeric: true,
+        width: 30,
+    },
+    {
+        id: 4,
+        label: 'Лимит',
+        numeric: true,
+        width: 40,
+    },
+    {
+        id: 5,
+        label: 'Дата прихода',
+        numeric: true,
+        width: 40,
+    },
+    {
+        id: 6,
+        label: 'Ред.',
+        numeric: true,
+        width: 40,
+    },
+ 
+]
+export const titlesFilialHeaderTable = [
+    {
+        id: 1,
+        label: '№',
+        numeric: true,
+        width: 5,
+    },
+    {
+        id:2,
+        label: 'Название кофейни',
+        numeric: true,
+        width: 10,
+    },
+    {
+        id: 3,
+        label: 'Адрес',
+        numeric: true,
+        width: 30,
+    },
+    {
+        id: 4,
+        label: 'Время работы',
+        numeric: true,
+        width: 40,
+    },
+    {
+        id: 5,
+        label: 'Ред.',
+        numeric: true,
+        width: 40,
+    },
+ 
+]
+export const titlesEmplayeesHeaderTable = [
+    {
+        id: 1,
+        label: '№',
+        numeric: true,
+        width: 5,
+    },
+    {
+        id:2,
+        label: 'Имя',
+        numeric: true,
+        width: 10,
+    },
+    {
+        id: 3,
+        label: 'Должность',
+        numeric: true,
+        width: 30,
+    },
+    {
+        id: 4,
+        label: 'Выберите филиал',
+        numeric: true,
+        width: 40,
+    },
+    {
+        id: 5,
+        label: 'Телефон',
+        numeric: true,
+        width: 40,
+    },
+    {
+        id: 6,
+        label: 'График работы',
+        numeric: true,
+        width: 40,
+    },
+    {
+        id: 7,
+        label: 'Ред.',
+        numeric: true,
+        width: 40,
+    },
+ 
+]
+export const data = [
+    {
+        id: 'one',
+        label: 'Капучино',
+        category: 'Кофе',
+        sostav: 'Молоко (70 мл), Кофе (15 зерен), Во...',
+        price: '100'
+    },
+    {
+        id: 'two',
+        label: 'Капучино',
+        category: 'Кофе',
+        sostav: 'Молоко (70 мл), Кофе (15 зерен), Во...',
+        price: '100'
+    },
+    {
+        id: 'three',
+        label: 'Капучино',
+        category: 'Кофе',
+        sostav: 'Молоко (70 мл), Кофе (15 зерен), Во...',
+        price: '100'
+    },
+    {
+        id: 'four',
+        label: 'Капучино',
+        category: 'Кофе',
+        sostav: 'Молоко (70 мл), Кофе (15 зерен), Во...',
+        price: '100'
+    },
+    {
+        id: 'five',
+        label: 'Капучино',
+        category: 'Кофе',
+        sostav: 'Молоко (70 мл), Кофе (15 зерен), Во...',
+        price: '100'
+    },
+    // {
+    //     id: 6,
+    //     label: 'График работы',
+    //     numeric: true,
+    //     width: 40,
+    // },
+    // {
+    //     id: 7,
+    //     label: 'Ред.',
+    //     numeric: true,
+    //     width: 40,
+    // },
+ 
+]
