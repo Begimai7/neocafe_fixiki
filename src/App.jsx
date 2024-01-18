ab7b89c6b6f356c5398014b2263cfe19bca3e1a3
unknown <sherbolot>
2024-01-18 13:53:42 +0600

    input

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-// import AuthAdmin from './components/admin/AdminAuth'
-import Input from '../src/components/UI/Input.jsx'
 
-function App( width,padding) {
+
+function App() {
     return (
-        <div width padding>
-            {/* <AuthAdmin /> */}
-            <Input width={width} />
-            <Input />
-            <Input width="600px" />
-            <Input  />
+        <div>
+
         </div>
     )
 }
