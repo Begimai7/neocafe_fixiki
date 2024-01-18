487614acd854fd51e01b3a715e06d76e2ee95bb6
Nur1es <nurlesnazirbaev1@gmail.com>
2024-01-18 12:51:47 +0600

    UI button

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
-import PermanentDrawerLeft from "./components/admin/AdminSideBar"
+import Button from "./components/admin/UI/button"
 
 function App() {
-    return <PermanentDrawerLeft/>
+    return <Button background = '#00315D'   borderRadius='6px'  padding = '16px 70px' color = 'white' >Сохронить</Button>
 }
 
 export default App
