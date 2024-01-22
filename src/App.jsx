05f5857e058c3175f19d06396725347bd6ffbf4d
Nurles <151987604+Nur1es@users.noreply.github.com>
2024-01-22 18:57:54 +0600

    Revert "Feature/filial"

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,7 @@
-import { useState } from "react";
-import ModalUnstyled from "./components/admin/AddNewFillial";
-
-// import MyForm from "./components/Test"
-
+import './App.css'
 
 function App() {
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-
-    return (
-        <div>
-            <button  onClick={handleOpen}>
-                Open modal
-            </button>  
-            {open ?  <ModalUnstyled handleClose={handleClose} /> :(
-                <h1>rdtcvgbhi</h1>  
-            )}
-            {/* <MyForm/> */}
-        </div>
-    )
+    return <p>hello</p>
 }
+
 export default App
