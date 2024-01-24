a467b73b459feea4262fe457c20fe16b825ff8e9
Begimai7 <begikeldibekova@gmail.com>
2024-01-24 14:45:58 +0600

    fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,4 @@
-import { useState } from "react";
-import ModalUnstyled from "./components/admin/AddNewFillial";
-
-// import MyForm from "./components/Test"
-
-
 function App() {
-    const [open, setOpen] = useState(false);
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-
-    return (
-        <div>
-            <button  onClick={handleOpen}>
-                Open modal
-            </button>  
-            {open ?  <ModalUnstyled handleClose={handleClose} /> :(
-                <h1>rdtcvgbhi</h1>  
-            )}
-            {/* <MyForm/> */}
-        </div>
-    )
+    return <div />
 }
 export default App
