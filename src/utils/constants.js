0318fe1e05ab06a6d41157ba2aa199489d8e8cde
unknown <sherbolot>
2024-02-05 15:03:05 +0600

    utils

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,6 @@ export const ROLES = {
     ADMIN: 'ADMIN',
     BARISTA: 'BARISTA',
 }
-
 export const titlesMenuHeaderTable = [
     {
         id: 'one',
@@ -167,7 +166,6 @@ export const titlesEmplayeesHeaderTable = [
         width: 40,
     },
 ]
-
 export const sidebarMenu = [
     {
         id: 1,
@@ -268,58 +266,6 @@ export const editSklad = [
     {
         id: '3',
         name: 'Сырье',
-    },
-]
-export const sidebarMenu = [
-    {
-        id: 1,
-        name: 'Меню',
-        path: '/menu',
-    },
-    {
-        id: 2,
-        name: 'Склад',
-        path: '/склад',
-    },
-    {
-        id: 1,
-        name: 'Филиалы',
-        path: '/филиалы',
-    },
-    {
-        id: 1,
-        name: 'Сотрудники',
-        path: '/сотрудники',
-    },
-]
-export const weekAddNewFilial = [
-    {
-        id: '1',
-        day: 'Понедельник',
-    },
-    {
-        id: '2',
-        day: 'Вторник',
-    },
-    {
-        id: '3',
-        day: 'Среда',
-    },
-    {
-        id: '4',
-        day: 'Четверг',
-    },
-    {
-        id: '5',
-        day: 'Пятница',
-    },
-    {
-        id: '6',
-        day: 'Суббота',
-    },
-    {
-        id: '7',
-        day: 'Воскресенье',
     },
 ]
 export const skladTabsTitle = [
